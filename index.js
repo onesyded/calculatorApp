b54175aa5f225568d0a657f028a930aebff3a785
onesyded <richmond006mensah@gmail.com>
2025-10-19 18:34:48 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-let curerent_input = "0";
+let current_input = "0";
 let previous_input = " ";
 let operator = " ";
+let shouldclear = true;
 const screen = document.getElementById("screen");
+
 function appendNumber(num) {
-    curerent_input = curerent_input + num;
-    screen.textContent = curerent_input;
+    if (shouldclear) {
+        current_input = num;
+        shouldclear = false;
+    }
+    
+    else{
+        current_input+= num;
+    }
+    updateScreen()
+
 }
 function appendOperator(op) {
-    curerent_input = previous_input;
+    previous_input=current_input;
     operator = op;
-    screen.textContent = operator;
+    current_input = operator;
+    shouldclear = true;
+    updateScreen()
+}
+function updateScreen() {
+   screen.textContent = current_input; 
+}
+function calculate() {
+    switch (operator) {
+        case '+':
+           current_input = Number(previous_input) + Number(current_input); 
+            break;
+        case '-':
+            current_input = Number(previous_input) - Number(current_input);
+            break;
+        case 'x':
+            current_input= Number(previous_input) * Number(current_input);
+            break;
+        case '/':
+            current_input = Number(previous_input) / Number(current_input);
+        default:
+            break;
+    }
+    updateScreen()
+}
+
+function clearscreen() {
+    previous_input = "";
+    current_input = "0";
+    operator = "";
+    shouldclear = true;
+    updateScreen()
 }
